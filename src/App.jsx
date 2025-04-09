88c4c547ae05ccbea0c5dcde7aa5f8b576200af9
Sofi Dobriak <167961175+sofi-dobriak@users.noreply.github.com>
2025-04-09 16:34:05 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Container from './components/Container/Container';
+
+function App() {
+    return (
+        <>
+            <Container></Container>
+        </>
+    );
+}
+
+export default App;
